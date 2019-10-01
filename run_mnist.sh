f685a5eaad41c7061819fad4c08a46b1d95e1ce0
PerryXDeng <pxd256@protonmail.com>
2019-10-01 17:45:29 -0400

    ??

--- run_mnist.sh
+++ run_mnist.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python3 train_val.py --dataset=mnist --batch_size=64 --A=16 --B=4 --C=8 --D=8 --num_gpus=1 --epoch=5 --load_dir=logs/mnist/test/20191001_15\:37\:50\:422185_/ --logdir=test
+python3 train_val.py --dataset=mnist --batch_size=64 --A=16 --B=4 --C=8 --D=8 --num_gpus=1 --epoch=15 --load_dir=logs/mnist/test/20191001_15\:37\:50\:422185_/ --logdir=test
 # python3 train_val.py --batch_size 8 --num_gpus 1 --epoch 1 --logdir test
