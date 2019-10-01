502d871571380d6d9681f263fd6392201048182f
PerryXDeng <pxd256@protonmail.com>
2019-10-01 16:09:30 -0400

    updated configs

--- run_mnist.sh
+++ run_mnist.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python3 train_val.py --dataset mnist --batch_size 64 --num_gpus 1 --epoch 1 --logdir test
+python3 train_val.py --dataset mnist --batch_size 64 --A 16 --B 4 --C 8 --D 8 --num_gpus 1 --epoch 5 --logdir test
 # python3 train_val.py --batch_size 8 --num_gpus 1 --epoch 1 --logdir test
